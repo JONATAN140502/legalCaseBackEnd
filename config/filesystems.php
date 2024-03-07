5804ea082e5028fba3c07d35fa7a767d7081ce6e
JONATAN140502 <jmayangamu@unprg.edu.pe>
2024-03-07 08:13:28 -0500

    disco

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,7 +46,7 @@ return [
         'public_server' => [
             'driver' => 'local',
             // 'root'   => base_path() .'/../sisgefiles/',
-            'root'   => base_path() .'/var/www/sisgefiles/',
+            'root'   =>'/../../sisgefiles/',
         ],
         's3' => [
             'driver' => 's3',
