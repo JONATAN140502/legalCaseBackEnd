f5c10ceb1451f5d05a108c96857b4da0c7e8a4c5
JONATAN140502 <jmayangamu@unprg.edu.pe>
2024-03-07 08:22:52 -0500

    oksi

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,7 +46,7 @@ return [
         'public_server' => [
             'driver' => 'local',
             // 'root'   => base_path() .'/../sisgefiles/',
-            'root'   => base_path('/../../sisgefiles/')
+             'root' => base_path('/../../sisgefiles/')
         ],
         's3' => [
             'driver' => 's3',
